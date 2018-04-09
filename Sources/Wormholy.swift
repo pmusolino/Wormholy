f8da3021e220c7d6da6f3524885ec9aa7d50cd2d
Paolo Musolino <info@codeido.com>
2018-04-09 16:25:51 +0200

    Project generated with swiftplate

---
+++ Sources/Wormholy.swift
@@ -0,0 +1,9 @@
+//
+//  Wormholy.swift
+//  Wormholy
+//
+//  Created by Paolo Musolino on {TODAY}.
+//  Copyright © 2018 Wormholy. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/WormholyTests/WormholyTests.swift
@@ -0,0 +1,23 @@
+//
+//  WormholyTests.swift
+//  Wormholy
+//
+//  Created by Paolo Musolino on {TODAY}.
+//  Copyright © 2018 Wormholy. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Wormholy
+
+class WormholyTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(Wormholy().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
