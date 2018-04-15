5031c7ade3c84c1636cac7cf6395fad0a1aae1e7
PAOLO MUSOLINO <info@codeido.com>
2018-04-15 20:47:46 +0200

    - Colors - Request Cell http code colors

--- Sources/UI/Cells/RequestCell.swift
+++ Sources/UI/Cells/RequestCell.swift
@@ -21,8 +21,27 @@ class RequestCell: UICollectionViewCell {
         }
         
         methodLabel.text = request?.method.uppercased()
+        codeLabel.isHidden = request?.code == 0 ? true : false
         codeLabel.text = request?.code != nil ? String(request!.code) : ""
+        if let code = request?.code{
+            var color: UIColor = Colors.HTTPCode.Generic
+            switch code {
+            case 200..<300:
+                color = Colors.HTTPCode.Success
+            case 300..<400:
+                color = Colors.HTTPCode.Redirect
+            case 400..<500:
+                color = Colors.HTTPCode.ClientError
+            case 500..<600:
+                color = Colors.HTTPCode.ServerError
+            default:
+                color = Colors.HTTPCode.Generic
+            }
+            codeLabel.borderColor = color
+            codeLabel.textColor = color
+        }
         urlLabel.text = request?.url
         durationLabel.text = request?.duration != nil ? String(request!.duration!) : ""
+        
     }
 }
