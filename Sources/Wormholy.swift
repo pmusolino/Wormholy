f85fabad5e27cd9752003dfa5772cac53b989256
Paolo Musolino <info@codeido.com>
2018-05-08 19:43:39 +0200

    Some tests

--- Sources/Wormholy.swift
+++ Sources/Wormholy.swift
@@ -19,9 +19,9 @@ public class Wormholy: NSObject
     }
     
     @objc public static func swiftyInitialize() {
-        if self == Wormholy.self{
+       // if self == Wormholy.self{
             Wormholy.enable(true)
-        }
+      //  }
     }
     
     static func enable(_ enable: Bool){
