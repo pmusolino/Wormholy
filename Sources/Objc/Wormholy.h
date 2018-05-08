140b201bf9d1a868ab5f4bebd9a135c54cb72e9f
Paolo Musolino <info@codeido.com>
2018-05-08 17:19:09 +0200

    Added Wormholy.h

---
+++ Sources/Objc/Wormholy.h
@@ -0,0 +1,13 @@
+//
+//  Wormholy.h
+//  Wormholy-iOS
+//
+//  Created by Paolo Musolino on 08/05/18.
+//  Copyright © 2018 Wormholy. All rights reserved.
+//
+
+#ifndef Wormholy_h
+#define Wormholy_h
+
+
+#endif /* Wormholy_h */
