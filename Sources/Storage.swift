787270bbfa3811709586b248fa5d0c67fefeb90a
Paolo Musolino <info@codeido.com>
2018-11-28 17:47:16 +0100

    Improvements

--- Sources/Storage.swift
+++ Sources/Storage.swift
@@ -19,10 +19,10 @@ open class Storage: NSObject {
             return
         }
         
-        if let index = requests.index(where: { (req) -> Bool in
+        if let index = requests.index(where: { [weak self] (req) -> Bool in
             return request?.id == req.id ? true : false
         }){
-            self.requests[index] = request!
+            requests[index] = request!
         }else{
             requests.insert(request!, at: 0)
         }
@@ -30,6 +30,6 @@ open class Storage: NSObject {
     }
 
     func clearRequests() {
-        requests = []
+        requests.removeAll()
     }
 }
