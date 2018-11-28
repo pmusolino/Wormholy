a47d835349940963cde6fb5e8bf96135544722b7
Paolo Musolino <info@codeido.com>
2018-11-28 18:15:09 +0100

    Added weak in dispatch

--- Sources/UI/RequestsViewController.swift
+++ Sources/UI/RequestsViewController.swift
@@ -26,7 +26,7 @@ class RequestsViewController: WHBaseViewController {
         
         filteredRequests = Storage.shared.requests
         NotificationCenter.default.addObserver(forName: newRequestNotification, object: nil, queue: nil) { [weak self] (notification) in
-            DispatchQueue.main.sync {
+            DispatchQueue.main.sync { [weak self] in
                 self?.filteredRequests = self?.filterRequests(text: self?.searchController?.searchBar.text) ?? []
                 self?.collectionView.reloadData()
             }
