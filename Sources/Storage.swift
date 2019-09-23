519195b6d03089069587cd54da2c93afa5d3abab
aezhov <andrey.ezhov@exness.com>
2019-09-23 12:41:23 +0300

    1. Some UI improvements for RequestCell. The number of lines for URL label now is 0. It helps to display a long URL. 2. Added filters in Storage. In my application, there are a lot of analytics requests. I don't want to see them in the debug menu. Now I can add filters for hosts.

--- Sources/Storage.swift
+++ Sources/Storage.swift
@@ -14,17 +14,23 @@ open class Storage: NSObject {
     
     open var requests: [RequestModel] = []
     
-    func saveRequest(request: RequestModel?){
-        guard request != nil else {
+    open var hostFilters: [String] = []
+    
+    func saveRequest(request: RequestModel?) {
+        guard let request = request else {
             return
         }
-        
+        if let url = URL(string: request.url), let host = url.host {
+            if hostFilters.firstIndex(of: host) != nil {
+                return
+            }
+        }
         if let index = requests.firstIndex(where: { (req) -> Bool in
-            return request?.id == req.id ? true : false
+            return request.id == req.id ? true : false
         }){
-            requests[index] = request!
+            requests[index] = request
         }else{
-            requests.insert(request!, at: 0)
+            requests.insert(request, at: 0)
         }
         NotificationCenter.default.post(name: newRequestNotification, object: nil)
     }
