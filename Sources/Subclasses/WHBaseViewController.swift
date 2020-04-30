e6b494e95fc729e09b349d3d77b7f6037d5a0647
farhad jebelli <jebelli.farhad@gmail.com>
2020-04-30 20:24:50 +0430

    set priority of presentedViewController in currentViewController func to high
    fixes issue when UITabBarController.topViewController is nil but its presenting sth

--- Sources/Subclasses/WHBaseViewController.swift
+++ Sources/Subclasses/WHBaseViewController.swift
@@ -43,7 +43,9 @@ extension UIViewController{
     static func currentViewController(_ viewController: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
         guard let viewController = viewController else { return nil }
         
-        if let viewController = viewController as? UINavigationController {
+        if let viewController = viewController.presentedViewController {
+            return currentViewController(viewController)
+        } else if let viewController = viewController as? UINavigationController {
             if let viewController = viewController.visibleViewController {
                 return currentViewController(viewController)
             } else {
@@ -55,8 +57,6 @@ extension UIViewController{
             } else {
                 return currentViewController(viewController.selectedViewController)
             }
-        } else if let viewController = viewController.presentedViewController {
-            return currentViewController(viewController)
         } else if viewController.children.count > 0 {
             return viewController.children[0]
         } else {
