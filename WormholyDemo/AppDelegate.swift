a217d6a529a57e1fdfc1d884f06afb866b480ec2
Paolo Musolino <info@codeido.com>
2020-09-07 12:19:31 +0200

    feat: changed the number of API requests stored inside the demo app

--- WormholyDemo/AppDelegate.swift
+++ WormholyDemo/AppDelegate.swift
@@ -16,8 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        Wormholy.limit = 5
+        
+        // Limit the number of API requests stored
+        Wormholy.limit = 10
+        
         return true
     }
 
