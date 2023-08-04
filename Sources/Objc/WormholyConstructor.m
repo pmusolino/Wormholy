695ccee03c595177ddc4b9356e2381254aae8eba
Paolo Musolino <info@codeido.com>
2023-08-04 14:15:42 +0200

    fixed a warning: WormholyConstructor.m:6:63 A function declaration without a prototype is deprecated in all versions of C

--- Sources/Objc/WormholyConstructor.m
+++ Sources/Objc/WormholyConstructor.m
@@ -3,7 +3,7 @@
 /// Library constructor to observe `UIApplicationDidFinishLaunchingNotification` immediately on app launch.
 /// Calls `applicationDidFinishLaunching` on Wormholy class to initialize Wormholy.
 /// This is an alternative to a +initialize in Objective-C.
-static void __attribute__ ((constructor)) wormholy_constructor() {
+static void __attribute__ ((constructor)) wormholy_constructor(void) {
     Class class = NSClassFromString(@"Wormholy.Wormholy");
     SEL selector = NSSelectorFromString(@"applicationDidFinishLaunching");
 
