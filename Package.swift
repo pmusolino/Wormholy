e5706ed691c16c40d0bb0823acf468abd442f882
IlyaGolovanov1298 <133504613+IlyaGolovanov1298@users.noreply.github.com>
2025-03-13 23:51:51 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Wormholy",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "Wormholy",
